47a380ebda70639d37b87928af7ea0f5a44803c2
nmelaert <nicolas.melaerts@icloud.com>
2025-03-10 18:01:01 +0100

    Set locale to English for consistent test behavior across environments

--- src/test/java/visitor/TestScientificNotationVisitor.java
+++ src/test/java/visitor/TestScientificNotationVisitor.java
@@ -12,7 +12,7 @@ class TestScientificNotationVisitor {
         MyNumber n = new MyNumber(1234);
         ScientificNotationVisitor visitor = new ScientificNotationVisitor(2);
         visitor.visit(n);
-        assertEquals("1,23E+03", visitor.getResult());
+        assertEquals("1.23E+03", visitor.getResult());
     }
 
     @Test
@@ -20,7 +20,7 @@ class TestScientificNotationVisitor {
         RealNumber n = new RealNumber(0.000123, 2);
         ScientificNotationVisitor visitor = new ScientificNotationVisitor(2);
         visitor.visit(n);
-        assertEquals("1,23E-04", visitor.getResult());
+        assertEquals("1.23E-04", visitor.getResult());
     }
 
     @Test
@@ -28,6 +28,6 @@ class TestScientificNotationVisitor {
         RealNumber n = new RealNumber(123456.789, 6);
         ScientificNotationVisitor visitor = new ScientificNotationVisitor();
         visitor.visit(n);
-        assertEquals("1,234568E+05", visitor.getResult());
+        assertEquals("1.234568E+05", visitor.getResult());
     }
 } 
