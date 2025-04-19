c28b128d862fc9e2edd7247679e7b9189a9e3c3f
LiocansG <nico6041@hotmail.be>
2025-04-19 21:40:47 +0200

    Fix decimal formatting in expected output of LinearEquationSolverTest

--- src/test/java/calculator/linear/LinearEquationSolverTest.java
+++ src/test/java/calculator/linear/LinearEquationSolverTest.java
@@ -17,7 +17,7 @@ class LinearEquationSolverTest {
         List<String> variables = List.of("x", "y");
         LinearEquationSystemExpression solution = LinearEquationSolver.solve(A, b, variables);
 
-        String expected = "(x = 1,6000, y = 1,6000)";
+        String expected = "(x = 1.6000, y = 1.6000)";
         assertEquals(expected, solution.toString());
     }
 
@@ -30,7 +30,7 @@ class LinearEquationSolverTest {
         List<String> variables = List.of("x", "y", "z");
         LinearEquationSystemExpression solution = LinearEquationSolver.solve(A, b, variables);
 
-        String expected = "(x = 7,9333, y = -6,2667, z = 4,2000)";
+        String expected = "(x = 7.9333, y = -6.2667, z = 4.2000)";
         assertEquals(expected, solution.toString());
     }
 
