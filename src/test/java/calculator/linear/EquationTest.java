56d56f01534daf4c9eddece24b80a26791375b65
LiocansG <nico6041@hotmail.be>
2025-04-19 20:23:33 +0200

    Moving Matrix test into the right folder

---
+++ src/test/java/calculator/linear/EquationTest.java
@@ -0,0 +1,16 @@
+package calculator.linear;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EquationTest {
+
+    @Test
+    void getLeft() {
+    }
+
+    @Test
+    void getRight() {
+    }
+}
