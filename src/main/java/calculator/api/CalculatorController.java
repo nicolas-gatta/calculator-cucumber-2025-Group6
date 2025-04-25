c52cfdcd57888b9fd6a6a402076cc53024782e33
AymelineG <aymelinegilou@hotmail.com>
2025-04-25 21:44:08 +0200

    Add Springboot and basic test to create an API

---
+++ src/main/java/calculator/api/CalculatorController.java
@@ -0,0 +1,21 @@
+package calculator.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CalculatorController {
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+}
