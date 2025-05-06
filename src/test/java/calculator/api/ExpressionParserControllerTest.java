d2bdda004f06d676b598d19ee4e2a720ceaece97
AymelineG <aymelinegilou@hotmail.com>
2025-05-06 22:11:13 +0200

    Add parser to API

--- src/test/java/calculator/api/ExpressionParserControllerTest.java
+++ src/test/java/calculator/api/ExpressionParserControllerTest.java
@@ -1,6 +1,5 @@
 package calculator.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,8 +15,6 @@ class ExpressionParserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Test
     void expressionParserTest() throws Exception {
