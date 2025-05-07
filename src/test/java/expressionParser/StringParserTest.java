3ccbc95f447ca72a72dd3cb7e3174fd2717e3c63
LiocansG <nico6041@hotmail.be>
2025-05-07 13:51:41 +0200

    Update StringParserTest.java

--- src/test/java/expressionParser/StringParserTest.java
+++ src/test/java/expressionParser/StringParserTest.java
@@ -24,7 +24,7 @@ class StringParserTest {
     }
 
     @Test
-    void testValidParenthesesExpression() {
+    void testInvalidParenthesesExpression() {
         String input = "(x + 2 = 5)";
         assertThrows(RecognitionException.class, () -> StringParser.parse(input));
     }
