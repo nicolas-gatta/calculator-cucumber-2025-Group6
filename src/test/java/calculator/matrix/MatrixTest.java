e826431f8f2e30818ee8d64fecd6e01f17a36ad7
AymelineG <aymelinegilou@hotmail.com>
2025-05-11 16:19:17 +0200

    Add test for matrix

--- src/test/java/calculator/matrix/MatrixTest.java
+++ src/test/java/calculator/matrix/MatrixTest.java
@@ -40,8 +40,22 @@ class MatrixTest {
     }
 
     @Test
+    void  testEmptyMatrix3() {
+        double[][] input = { new double[0] };
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(input));
+    }
+
+    @Test
     void testInvalidParseMatrix() {
         assertThrows(IllegalArgumentException.class, () -> new Matrix("[1,2,3]"));
+    }
+
+    @Test
+    void testInvalidParseMatrix2() {assertThrows(IllegalArgumentException.class, () -> new Matrix("[[1,2,3]"));}
+
+    @Test
+    void testInconsistentColumnSizes() {
+        assertThrows(IllegalArgumentException.class, () -> new Matrix("[[1,2],[3]]"));
     }
 
     @Test
@@ -93,6 +107,13 @@ class MatrixTest {
     }
 
     @Test
+    void testInvalidAddition2() {
+        Matrix m1 = new Matrix("[[1,2]]");
+        Matrix m2 = new Matrix("[[1,2,3]]");
+        assertThrows(IllegalArgumentException.class, () -> m1.add(m2));
+    }
+
+    @Test
     void testSubtraction2x2() {
         Matrix m1 = new Matrix("[[5,6],[7,8]]");
         Matrix m2 = new Matrix("[[1,2],[3,4]]");
@@ -112,6 +133,13 @@ class MatrixTest {
     void testInvalidSubtraction() {
         Matrix m1 = new Matrix("[[5,6],[7,8]]");
         Matrix m2 = new Matrix("[[1,2,3],[4,5,6],[7,8,9]]");
+        assertThrows(IllegalArgumentException.class, () -> m1.subtract(m2));
+    }
+
+    @Test
+    void testInvalidSubtraction2() {
+        Matrix m1 = new Matrix("[[1,2]]");
+        Matrix m2 = new Matrix("[[1,2,3]]");
         assertThrows(IllegalArgumentException.class, () -> m1.subtract(m2));
     }
 
@@ -223,6 +251,12 @@ class MatrixTest {
     @Test
     void testInvalidInverse() {
         Matrix m = new Matrix("[[3,0],[2,0]]");
+        assertThrows(IllegalArgumentException.class, m::inverse);
+    }
+
+    @Test
+    void testInvalidInverse2() {
+        Matrix m = new Matrix("[[1, 2, 3], [4, 5, 6]]");
         assertThrows(IllegalArgumentException.class, m::inverse);
     }
 
