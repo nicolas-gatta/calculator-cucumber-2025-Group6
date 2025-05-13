a260422fb7dcb3ae23de1820b530f53f663b437b
LiocansG <nico6041@hotmail.be>
2025-05-13 11:45:03 +0200

    Update ApplicationCLITest.java

--- src/test/java/calculator/cli/ApplicationCLITest.java
+++ src/test/java/calculator/cli/ApplicationCLITest.java
@@ -1,125 +1,89 @@
 package calculator.cli;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import unit_converter.enums.EnumDisplayUtil;
+import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ApplicationCLITest {
 
-public class ApplicationCLITest {
-
-    private final PrintStream standardOut = System.out;
+    private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @AfterEach
-    public void restoreSystemOut() {
-        System.setOut(standardOut);
+    void teardown() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+        outputStreamCaptor.reset();
     }
 
     @Test
+    @Order(2)
     void testHandleCommandReturnsTrueOnExit() {
         assertTrue(ApplicationCLI.handleCommand("exit", "some help"));
     }
 
     @Test
+    @Order(3)
     void testHandleCommandDisplaysHelp() {
         assertFalse(ApplicationCLI.handleCommand("help", "Expected help message here"));
     }
 
     @Test
+    @Order(4)
     void testHandleCommandReturnsFalseOnEmptyInput() {
         assertFalse(ApplicationCLI.handleCommand("", "help"));
     }
 
     @Test
+    @Order(5)
     void testHandleCommandReturnsFalseOnInputWithSpaces() {
         assertFalse(ApplicationCLI.handleCommand(" ", "help"));
     }
 
 
     @Test
+    @Order(6)
     void testHandleCommandReturnsFalseOnWrongInput() {
         assertFalse(ApplicationCLI.handleCommand("blablabla", "help"));
     }
 
 
     @Test
+    @Order(7)
     void testPrivateConstructorThrowsException() {
         assertThrows(IllegalStateException.class, ApplicationCLI::new);
     }
 
     @Test
-    void testEntryPointHelp(){
+    @Order(1)
+    void testEntryPoint(){
 
-        String simulatedUserInput = "help\nexit\n";
+        String simulatedUserInput = "help\nblabla\ncalculator\nhelp\n[[]]\n5+8\nexit\nconverter\nhelp\nblabla\nLength\ncentimeter\nmeter\n80\nexit\nexit\n";
         ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedUserInput.getBytes());
 
         System.setIn(inputStream);
 
         ApplicationCLI.entryPointCli();
 
-        System.setIn(System.in);
-
         String consoleOutput = outputStreamCaptor.toString();
-
-        System.out.println(consoleOutput);
 
         assertTrue(consoleOutput.contains(ApplicationCLI.TOOL_CHOOSER_MESSAGE));
         assertTrue(consoleOutput.contains("Choose your tool"));
         assertTrue(consoleOutput.contains("> "));
         assertTrue(consoleOutput.contains(ApplicationCLI.MAIN_HELP_MESSAGE));
-        assertTrue(consoleOutput.contains("Exit Application"));
-    }
-
-    @Test
-    void testEntryPointInvalid(){
-
-        String simulatedUserInput = "blabla\nexit\n";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedUserInput.getBytes());
-
-        System.setIn(inputStream);
-
-        ApplicationCLI.entryPointCli();
-
-        System.setIn(System.in);
-
-        String consoleOutput = outputStreamCaptor.toString();
-
-        assertTrue(consoleOutput.contains(ApplicationCLI.TOOL_CHOOSER_MESSAGE));
-        assertTrue(consoleOutput.contains("Choose your tool"));
-        assertTrue(consoleOutput.contains("> "));
         assertTrue(consoleOutput.contains("Unknown command. Type 'help' or 'exit'."));
-        assertTrue(consoleOutput.contains("Exit Application"));
-    }
-
-    @Test
-    void testEntryPointCalculator(){
-
-        String simulatedUserInput = "calculator\nhelp\n[[]]\n5+8\nexit\nexit\n";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedUserInput.getBytes());
-
-        System.setIn(inputStream);
-
-        ApplicationCLI.entryPointCli();
-
-        System.setIn(System.in);
-
-        String consoleOutput = outputStreamCaptor.toString();
-
-        assertTrue(consoleOutput.contains(ApplicationCLI.TOOL_CHOOSER_MESSAGE));
-        assertTrue(consoleOutput.contains("Choose your tool"));
-        assertTrue(consoleOutput.contains("> "));
         assertTrue(consoleOutput.contains(ApplicationCLI.CALCULATOR_MESSAGE));
         assertTrue(consoleOutput.contains(ApplicationCLI.CALCULATOR_HELP_MESSAGE));
         assertTrue(consoleOutput.contains("Enter your expression"));
@@ -127,27 +91,6 @@ public class ApplicationCLITest {
         assertTrue(consoleOutput.contains("Error: Invalid expression."));
         assertTrue(consoleOutput.contains("The result of evaluating expression (5 + 8)"));
         assertTrue(consoleOutput.contains("is: 13."));
-        assertTrue(consoleOutput.contains("Exit Application"));
-
-    }
-
-    @Test
-    void testEntryPointConverter(){
-
-        String simulatedUserInput = "converter\nhelp\nblabla\nLength\ncentimeter\nmeter\n80\nexit\nexit\n";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedUserInput.getBytes());
-
-        System.setIn(inputStream);
-
-        ApplicationCLI.entryPointCli();
-
-        System.setIn(System.in);
-
-        String consoleOutput = outputStreamCaptor.toString();
-
-        assertTrue(consoleOutput.contains(ApplicationCLI.TOOL_CHOOSER_MESSAGE));
-        assertTrue(consoleOutput.contains("Choose your tool"));
-        assertTrue(consoleOutput.contains("> "));
         assertTrue(consoleOutput.contains(ApplicationCLI.CONVERTER_MESSAGE));
         assertTrue(consoleOutput.contains("Enter conversion type"));
         assertTrue(consoleOutput.contains("> "));
